5c79250f77ce7fdf86fd2eb4ff9d3b060018d9ad
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2024-01-23 16:00:05 +0100

    adding eventlistener to "random"- button

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const renderBtn = document.getElementById("render-btn")
+
+const randomMenuBtn = document.getElementById("random-menu-btn")
 const inputField = document.getElementById("input-field")
+const renderBtn = document.getElementById("render-btn")
+
+
+
+randomMenuBtn.addEventListener("click", function(){
+    console.log("Random menu")
+})
 
 
 renderBtn.addEventListener("click", function(){
