e02b757f991b838ab376da9c99e10b1455ce215d
Emelie Rosendahl <89969808+Erm0es@users.noreply.github.com>
2024-01-24 14:16:41 +0100

    Fetching data from firebase

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://weeklymenu-9c4d9-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -26,6 +26,11 @@ renderBtn.addEventListener("click", function () {
     clearInputField()
     appendItemToOwnDishUl(inputValue)
 
+})
+
+onValue(ownDishInDB, function(snapshot){
+    let ownDishArr = Object.values(snapshot.val())
+    console.log(ownDishArr)
 })
 
 function clearInputField(){
